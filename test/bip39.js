39e2d7fa6c47cedf0fc84a278345761e794e6b88
Ryan X. Charles <ryanxcharles@gmail.com>
2014-11-13 09:22:32 -0800

    forEach is a more appropriate way to loop here

--- test/bip39.js
+++ test/bip39.js
@@ -16,23 +16,19 @@ describe('BIP39', function() {
     var phrase = BIP39.mnemonic(BIP39.wordlist_en, 128);
   });
 
-  en_vectors = vectors.english;
-  for (var v = 0 ; v < en_vectors.length ; v++) {
-    (function(v){
-      if (!process.env.TEST_SKIP_SLOW) {
-        it('should pass test vector ' + v, function() {
-          var vector = en_vectors[v];
-          var code = vector[0];
-          var mnemonic = vector[1];
-          var seed = vector[2];
-          var mnemonic1 = BIP39.entropy2mnemonic(BIP39.wordlist_en, new Buffer(code, 'hex'));
-          var seed1 = BIP39.mnemonic2seed(mnemonic, 'TREZOR');
-          BIP39.check(BIP39.wordlist_en, mnemonic).should.be.true;
-          mnemonic1.should.equal(mnemonic);
-          seed1.toString('hex').should.equal(seed)
-        });
-      }
-    })(v);
-  }
+  vectors.english.forEach(function(vector, v) {
+    if (!process.env.TEST_SKIP_SLOW) {
+      it('should pass test vector ' + v, function() {
+        var code = vector[0];
+        var mnemonic = vector[1];
+        var seed = vector[2];
+        var mnemonic1 = BIP39.entropy2mnemonic(BIP39.wordlist_en, new Buffer(code, 'hex'));
+        var seed1 = BIP39.mnemonic2seed(mnemonic, 'TREZOR');
+        BIP39.check(BIP39.wordlist_en, mnemonic).should.be.true;
+        mnemonic1.should.equal(mnemonic);
+        seed1.toString('hex').should.equal(seed)
+      });
+    }
+  });
 
 });
