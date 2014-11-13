a7c22bee240b1cd2e6d837b616f7163ecda00951
Ryan X. Charles <ryanxcharles@gmail.com>
2014-11-12 22:25:05 -0800

    allow skipping of slow tests
    run the tests with:
    
    TEST_SKIP_SLOW=1 mocha

--- test/bip39.js
+++ test/bip39.js
@@ -143,17 +143,19 @@ describe('BIP39', function() {
   var vectors = bip39_vectors['english'];
   for (var v = 0 ; v < vectors.length ; v++) {
     (function(v){
-      it('should pass test vector ' + v, function() {
-        var vector = vectors[v];
-        var code = vector[0];
-        var mnemonic = vector[1];
-        var seed = vector[2];
-        var mnemonic1 = BIP39.entropy2mnemonic(BIP39.wordlist_en, new Buffer(code, 'hex'));
-        var seed1 = BIP39.mnemonic2seed(mnemonic, 'TREZOR');
-        BIP39.check(BIP39.wordlist_en, mnemonic).should.be.true;
-        mnemonic1.should.equal(mnemonic);
-        seed1.toString('hex').should.equal(seed)
-      });
+      if (!process.env.TEST_SKIP_SLOW) {
+        it('should pass test vector ' + v, function() {
+          var vector = vectors[v];
+          var code = vector[0];
+          var mnemonic = vector[1];
+          var seed = vector[2];
+          var mnemonic1 = BIP39.entropy2mnemonic(BIP39.wordlist_en, new Buffer(code, 'hex'));
+          var seed1 = BIP39.mnemonic2seed(mnemonic, 'TREZOR');
+          BIP39.check(BIP39.wordlist_en, mnemonic).should.be.true;
+          mnemonic1.should.equal(mnemonic);
+          seed1.toString('hex').should.equal(seed)
+        });
+      }
     })(v);
   }
 
