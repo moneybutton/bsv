ad4876287e2813a2b70ff3f56e4405cc7c3d8ba5
Ryan X. Charles <ryanxcharles@gmail.com>
2014-11-20 17:00:51 -0800

    make all script chunks the same type
    It is slightly confusing and inconsistent that script chunks can either be a
    number or an object. I did it that way because BitcoinJS+bitcore did it that
    way. However, I find it to be more intuitive if they are all the same type. And
    that type is just an object with an opcodenum property, and optionally a
    buffer, and optionally also a len, for PUSHDATA ops where the len may or may
    not be equivalent to the buf.length (whether this ever occurs in the blockchain
    I don't know, but it is a logical possibility).

--- test/script.js
+++ test/script.js
@@ -19,7 +19,7 @@ describe('Script', function() {
       buf[0] = Opcode('OP_0').toNumber();
       var script = Script().fromBuffer(buf);
       script.chunks.length.should.equal(1);
-      script.chunks[0].should.equal(buf[0]);
+      script.chunks[0].opcodenum.should.equal(buf[0]);
     });
 
     it('should parse this buffer containing another OP code', function() {
@@ -27,7 +27,7 @@ describe('Script', function() {
       buf[0] = Opcode('OP_CHECKMULTISIG').toNumber();
       var script = Script().fromBuffer(buf);
       script.chunks.length.should.equal(1);
-      script.chunks[0].should.equal(buf[0]);
+      script.chunks[0].opcodenum.should.equal(buf[0]);
     });
 
     it('should parse this buffer containing three bytes of data', function() {
@@ -72,9 +72,9 @@ describe('Script', function() {
       buf[buf.length - 1] = Opcode('OP_0').toNumber();
       var script = Script().fromBuffer(buf);
       script.chunks.length.should.equal(3);
-      script.chunks[0].should.equal(buf[0]);
+      script.chunks[0].opcodenum.should.equal(buf[0]);
       script.chunks[1].buf.toString('hex').should.equal('010203');
-      script.chunks[2].should.equal(buf[buf.length - 1]);
+      script.chunks[2].opcodenum.should.equal(buf[buf.length - 1]);
     });
 
   });
@@ -86,7 +86,7 @@ describe('Script', function() {
       buf[0] = Opcode('OP_0').toNumber();
       var script = Script().fromBuffer(buf);
       script.chunks.length.should.equal(1);
-      script.chunks[0].should.equal(buf[0]);
+      script.chunks[0].opcodenum.should.equal(buf[0]);
       script.toBuffer().toString('hex').should.equal(buf.toString('hex'));
     });
 
@@ -95,7 +95,7 @@ describe('Script', function() {
       buf[0] = Opcode('OP_CHECKMULTISIG').toNumber();
       var script = Script().fromBuffer(buf);
       script.chunks.length.should.equal(1);
-      script.chunks[0].should.equal(buf[0]);
+      script.chunks[0].opcodenum.should.equal(buf[0]);
       script.toBuffer().toString('hex').should.equal(buf.toString('hex'));
     });
 
@@ -145,9 +145,9 @@ describe('Script', function() {
       buf[buf.length - 1] = Opcode('OP_0').toNumber();
       var script = Script().fromBuffer(buf);
       script.chunks.length.should.equal(3);
-      script.chunks[0].should.equal(buf[0]);
+      script.chunks[0].opcodenum.should.equal(buf[0]);
       script.chunks[1].buf.toString('hex').should.equal('010203');
-      script.chunks[2].should.equal(buf[buf.length - 1]);
+      script.chunks[2].opcodenum.should.equal(buf[buf.length - 1]);
       script.toBuffer().toString('hex').should.equal(buf.toString('hex'));
     });
 
@@ -175,9 +175,9 @@ describe('Script', function() {
       buf[buf.length - 1] = Opcode('OP_0').toNumber();
       var script = Script().fromBuffer(buf);
       script.chunks.length.should.equal(3);
-      script.chunks[0].should.equal(buf[0]);
+      script.chunks[0].opcodenum.should.equal(buf[0]);
       script.chunks[1].buf.toString('hex').should.equal('010203');
-      script.chunks[2].should.equal(buf[buf.length - 1]);
+      script.chunks[2].opcodenum.should.equal(buf[buf.length - 1]);
       script.toString().toString('hex').should.equal('OP_0 OP_PUSHDATA4 3 0x010203 OP_0');
     });
 
