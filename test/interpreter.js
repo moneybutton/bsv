d9fb6f6c5dae31558d0885430a5dab5a8cc0a94a
Ryan X. Charles <ryanxcharles@gmail.com>
2014-11-28 15:39:37 -0800

    script interpreter
    The script interpreter has been translated from bitcoin core's C++ code. It has
    been kept as similar as possible in order to be reasonably sure that it has
    been copied correctly. Note that as of this commit, the translation has not
    been tested against bitcoin core. Only extremely trivial tests that ensure that
    nothing terribly wrong happens have been applied.
    
    There are three relevant json files in bitcoin core that we will need to test
    against:
    
    script_valid.json
    script_invalid.json
    tx_valid.json
    tx_invalid.json
    
    Once we test against these files, and (likely) fix any bugs that will occur, we
    can have greater confidence that the script interpreter is correct.
    
    The commit of bitcoin core that this interpreter has been translated from is
    this: b5d1b1092998bc95313856d535c632ea5a8f9104

---
+++ test/interpreter.js
@@ -0,0 +1,46 @@
+var should = require('chai').should();
+var Interpreter = require('../lib/interpreter');
+var Tx = require('../lib/tx');
+var Script = require('../lib/script');
+
+describe('Interpreter', function() {
+
+  it('should make a new interpreter', function() {
+    var interpreter = new Interpreter();
+    (interpreter instanceof Interpreter).should.equal(true);
+    interpreter.stack.length.should.equal(0);
+    interpreter.altstack.length.should.equal(0);
+    interpreter.pc.should.equal(0);
+    interpreter.pbegincodehash.should.equal(0);
+    interpreter.nOpCount.should.equal(0);
+    interpreter.vfExec.length.should.equal(0);
+    interpreter.errstr.should.equal("");
+    interpreter.flags.should.equal(0);
+    var interpreter = Interpreter();
+    (interpreter instanceof Interpreter).should.equal(true);
+    interpreter.stack.length.should.equal(0);
+    interpreter.altstack.length.should.equal(0);
+    interpreter.pc.should.equal(0);
+    interpreter.pbegincodehash.should.equal(0);
+    interpreter.nOpCount.should.equal(0);
+    interpreter.vfExec.length.should.equal(0);
+    interpreter.errstr.should.equal("");
+    interpreter.flags.should.equal(0);
+  });
+
+  describe('#verify', function() {
+
+    it('should verify or unverify these trivial scripts', function() {
+      var verified = Interpreter().verify(Script('OP_1'), Script('OP_1'), Tx(), 0);
+      verified.should.equal(true);
+      var verified = Interpreter().verify(Script('OP_1'), Script('OP_0'), Tx(), 0);
+      verified.should.equal(false);
+      var verified = Interpreter().verify(Script('OP_0'), Script('OP_1'), Tx(), 0);
+      verified.should.equal(true);
+      var verified = Interpreter().verify(Script('OP_CODESEPARATOR'), Script('OP_1'), Tx(), 0);
+      verified.should.equal(true);
+    });
+
+  });
+
+});
