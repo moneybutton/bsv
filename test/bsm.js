465aa1dc02b9c47a2c6ee716fce03663ce7c1d2f
Ryan X. Charles <ryanxcharles@gmail.com>
2015-01-02 11:48:27 -0800

    Message -> BSM
    The name "Message", which referred to a Bitcoin Signed Message, was too
    confusingly named since it implies a "p2p message" even though it has nothing
    to do with the p2p messages of bitcoin. Thus it has been renamed to "BSM",
    short for "Bitcoin Signed Message", so to be less confusing.

--- test/bsm.js
+++ test/bsm.js
@@ -1,25 +1,25 @@
 var Address = require('../lib/address');
-var Message = require('../lib/message');
+var BSM = require('../lib/bsm');
 var Keypair = require('../lib/keypair');
 var should = require('chai').should();
 
-describe('Message', function() {
+describe('BSM', function() {
   
-  it('should make a new message', function() {
-    var message = new Message();
-    should.exist(message);
+  it('should make a new bsm', function() {
+    var bsm = new BSM();
+    should.exist(bsm);
   });
 
-  it('should make a new message when called without "new"', function() {
-    var message = Message();
-    should.exist(message);
+  it('should make a new bsm when called without "new"', function() {
+    var bsm = BSM();
+    should.exist(bsm);
   });
   
   describe('#set', function() {
 
     it('should set the messagebuf', function() {
       var messagebuf = new Buffer('message');
-      should.exist(Message().set({messagebuf: messagebuf}).messagebuf);
+      should.exist(BSM().set({messagebuf: messagebuf}).messagebuf);
     });
 
   });
@@ -29,7 +29,7 @@ describe('Message', function() {
     var keypair = Keypair().fromRandom();
 
     it('should return a base64 string', function() {
-      var sigstr = Message.sign(messagebuf, keypair);
+      var sigstr = BSM.sign(messagebuf, keypair);
       var sigbuf = new Buffer(sigstr, 'base64');
       sigbuf.length.should.equal(1 + 32 + 32);
     });
@@ -37,7 +37,7 @@ describe('Message', function() {
     it('should sign with a compressed pubkey', function() {
       var keypair = Keypair().fromRandom();
       keypair.pubkey.compressed = true;
-      var sigstr = Message.sign(messagebuf, keypair);
+      var sigstr = BSM.sign(messagebuf, keypair);
       var sigbuf = new Buffer(sigstr, 'base64');
       sigbuf[0].should.be.above(27 + 4 - 1);
       sigbuf[0].should.be.below(27 + 4 + 4 - 1);
@@ -46,7 +46,7 @@ describe('Message', function() {
     it('should sign with an uncompressed pubkey', function() {
       var keypair = Keypair().fromRandom();
       keypair.pubkey.compressed = false;
-      var sigstr = Message.sign(messagebuf, keypair);
+      var sigstr = BSM.sign(messagebuf, keypair);
       var sigbuf = new Buffer(sigstr, 'base64');
       sigbuf[0].should.be.above(27 - 1);
       sigbuf[0].should.be.below(27 + 4 - 1);
@@ -59,16 +59,16 @@ describe('Message', function() {
     var keypair = Keypair().fromRandom();
 
     it('should verify a signed message', function() {
-      var sigstr = Message.sign(messagebuf, keypair);
+      var sigstr = BSM.sign(messagebuf, keypair);
       var addr = Address().fromPubkey(keypair.pubkey);
-      Message.verify(messagebuf, sigstr, addr).should.equal(true);
+      BSM.verify(messagebuf, sigstr, addr).should.equal(true);
     });
 
     it('should verify this known good signature', function() {
       var addrstr = '1CKTmxj6DjGrGTfbZzVxnY4Besbv8oxSZb';
       var address = Address().fromString(addrstr);
       var sigstr = 'IOrTlbNBI0QO990xOw4HAjnvRl/1zR+oBMS6HOjJgfJqXp/1EnFrcJly0UcNelqJNIAH4f0abxOZiSpYmenMH4M=';
-      Message.verify(messagebuf, sigstr, address);
+      BSM.verify(messagebuf, sigstr, address);
     });
 
   });
@@ -78,11 +78,11 @@ describe('Message', function() {
     var keypair = Keypair().fromRandom();
 
     it('should sign a message', function() {
-      var message = new Message();
-      message.messagebuf = messagebuf;
-      message.keypair = keypair;
-      message.sign();
-      var sig = message.sig;
+      var bsm = new BSM();
+      bsm.messagebuf = messagebuf;
+      bsm.keypair = keypair;
+      bsm.sign();
+      var sig = bsm.sig;
       should.exist(sig);
     });
 
@@ -93,13 +93,13 @@ describe('Message', function() {
     var keypair = Keypair().fromRandom();
 
     it('should verify a message that was just signed', function() {
-      var message = new Message();
-      message.messagebuf = messagebuf;
-      message.keypair = keypair;
-      message.address = Address().fromPubkey(keypair.pubkey);
-      message.sign();
-      message.verify();
-      message.verified.should.equal(true);
+      var bsm = new BSM();
+      bsm.messagebuf = messagebuf;
+      bsm.keypair = keypair;
+      bsm.address = Address().fromPubkey(keypair.pubkey);
+      bsm.sign();
+      bsm.verify();
+      bsm.verified.should.equal(true);
     });
 
   });
