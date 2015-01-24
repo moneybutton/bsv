c8260f61e29a9c155672eec55f5e36826524f535
Braydon Fuller <braydon@bitpay.com>
2015-01-23 21:33:15 -0500

    Fixed issue where the sender's publickey in the encrypted buffer was not recognized.

--- test/ecies.js
+++ test/ecies.js
@@ -47,10 +47,14 @@ describe('ECIES', function() {
   var alice = ECIES()
     .privateKey(aliceKey)
     .publicKey(bobKey.publicKey);
+
   var bob = ECIES()
     .privateKey(bobKey)
     .publicKey(aliceKey.publicKey);
+
   var message = 'attack at dawn';
+  var encrypted = '0339e504d6492b082da96e11e8f039796b06cd4855c101e2492a6f10f3e056a9e7499368e41313fa48f71759d13469c28b0bd6ff03a08b2ae5e6679e848f92db4b26a8ccf9c0b43f16eae6216c36380f70724743fe9053c98d94281aa74c94df40';
+  var encBuf = new Buffer(encrypted, 'hex');
 
   it('correctly encrypts a message', function() {
     var encrypted = alice.encrypt(message);
@@ -58,14 +62,16 @@ describe('ECIES', function() {
   });
 
   it('correctly decrypts a message', function() {
-
-    var encrypted = '0339e504d6492b082da96e11e8f039796b06cd4855c101e2492a6f10f3e056a9e7499368e41313fa48f71759d13469c28b0bd6ff03a08b2ae5e6679e848f92db4b26a8ccf9c0b43f16eae6216c36380f70724743fe9053c98d94281aa74c94df40';
-
-    var encBuf = new Buffer(encrypted, 'hex');
     var decrypted = bob
       .decrypt(encBuf)
       .toString();
+    decrypted.should.equal(message);
+  });
 
+  it('retrieves senders publickey from the encypted buffer', function() {
+    var bob2 = ECIES().privateKey(bobKey);
+    var decrypted = bob2.decrypt(encBuf).toString();
+    bob2._publicKey.toDER().should.deep.equal(aliceKey.publicKey.toDER());
     decrypted.should.equal(message);
   });
 
