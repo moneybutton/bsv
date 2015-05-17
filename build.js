fc8ca98e0407b7bf20867fd08c2edaa7569d5bb3
Ryan X. Charles <ryanxcharles@gmail.com>
2015-05-16 23:10:53 -0700

    use Map in Txoutmap, and many related changes
    In order to continue migrating to the use of ES6 throughout fullnode, I wanted
    to use Map in Txoutmap, which seemed natural. This presented several challanges
    largely unrelated to the use of Map, but related to the way I'm building the
    library and testing it in the browser. Thus I had to make several other changes
    besides just updating Txoutmap:
    
    - Change the browserify build script to be a distinct script called build.js
      rather than a fancy long shell command in package.json. The advantage here is
      that now I can be more specific about what options I'm using for browserify
      and, in particular, the es6ify transform.
    
    - I must "add" es6ify.runtime to be able to use Map in the browser.
    
    - I must exclude all dependencies from the es6ify transform since they don't
      need to be transformed, and one in particular, bn.js, created issues when
      running through the transform.
    
    - I've added the "glob" dependency to find all the individual files that need
      to be tested so I can browserify them (I wasn't able to find a feature of
      browserify to find files matching a pattern, e.g. "*.js", but glob is perfect
      for this).
    
    - Use istanbul-harmony, which is a fork of istanbul that supports "node
      --harmony" features, which was necessary for it to understand the use of Map.
    
    - Remove the examples and their associated tests, which I don't have time to
      support, and which created issues with all the changes to the tests.

---
+++ build.js
@@ -0,0 +1,45 @@
+"use strict";
+
+let path = require('path');
+let fs = require('fs');
+let browserify = require('browserify');
+let es6ify = require('es6ify');
+let uglifyify = require('uglifyify');
+let glob = require('glob');
+let fullPath = path.join(__dirname, 'browser', 'fullnode.js');
+let minPath = path.join(__dirname, 'browser', 'fullnode-min.js');
+let testPath = path.join(__dirname, 'browser', 'tests.js');
+
+// fullnode.js
+browserify({debug: false})
+.add(es6ify.runtime)
+// es6ify all files except those in node_modules
+.transform(es6ify.configure(/^(?!.*node_modules)+.+\.js$/))
+.require(require.resolve('./index.js'), {entry: true})
+.bundle()
+.on('error', function (err) { console.error(err); })
+.pipe(fs.createWriteStream(fullPath));
+
+// fullnode-min.js
+browserify({debug: false})
+.add(es6ify.runtime)
+.transform(es6ify.configure(/^(?!.*node_modules)+.+\.js$/))
+.transform(uglifyify)
+.require(require.resolve('./index.js'), {entry: true})
+.bundle()
+.on('error', function (err) { console.error(err); })
+.pipe(fs.createWriteStream(minPath));
+
+// tests
+glob("./test/**/*.js", {}, function (err, files) {
+  let b = browserify({debug: true})
+  .add(es6ify.runtime)
+  .transform(es6ify.configure(/^(?!.*node_modules)+.+\.js$/))
+  .transform(uglifyify);
+  for (let file of files) {
+    b.add(file);
+  }
+  b.bundle()
+  .on('error', function (err) { console.error(err); })
+  .pipe(fs.createWriteStream(testPath));
+});
--- test/txoutmap.js
+++ test/txoutmap.js
@@ -11,14 +11,14 @@ describe('Txoutmap', function() {
   txhashbuf.fill(0);
   let label = txhashbuf.toString('hex') + ':' + '0';
   let txout = Txout(BN(0), Script('OP_RETURN'));
-  let map = {};
-  map[label] = txout;
+  let map = new Map();
+  map.set(label, txout);
   let tx = Tx().fromHex('0100000001795b88d47a74e3be0948fc9d1b4737f96097474d57151afa6f77c787961e47cc120000006a47304402202289f9e1ae2ed981cd0bf62f822f6ae4aea40c65c7339d90643cea90de93ad1502205c8a08b3265f9ba7e99057d030d5b91c889a1b99f94a3a5b79d7daaada2409b6012103798b51f980e7a3690af6b43ce3467db75bede190385702c4d9d48c0a735ff4a9ffffffff01c0a83200000000001976a91447b8e62e008f82d95d1f565055a8243cc243d32388ac00000000');
 
   it('should make a new txoutmap', function() {
     let txoutmap = new Txoutmap();
     txoutmap = Txoutmap();
-    txoutmap = Txoutmap(map);
+    txoutmap = Txoutmap({map: map});
     should.exist(txoutmap);
     should.exist(txoutmap.map);
   });
@@ -27,15 +27,7 @@ describe('Txoutmap', function() {
 
     it('should set a map', function() {
       let txoutmap = Txoutmap().set({map: map});
-      txoutmap.map[label].toHex().should.equal(txout.toHex());
-    });
-
-    it('should not set prototype items of a map', function() {
-      let map2 = Object.create({'fake': true});
-      map2[label] = map[label];
-      should.exist(map2.fake);
-      let txoutmap = Txoutmap().set({map: map2});
-      should.not.exist(txoutmap.map.fake);
+      txoutmap.map.get(label).toHex().should.equal(txout.toHex());
     });
 
   });
@@ -44,7 +36,7 @@ describe('Txoutmap', function() {
     
     it('should add a txout to the txoutmap', function() {
       let txoutmap = Txoutmap().add(txhashbuf, 0, txout);
-      should.exist(txoutmap.map[label]);
+      should.exist(txoutmap.map.get(label));
     });
 
   });
