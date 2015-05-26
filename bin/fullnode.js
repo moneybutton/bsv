5fddd4b41378ab7bbec3a3ee61edc3738be6018f
Ryan X. Charles <ryanxcharles@gmail.com>
2015-05-25 19:38:36 -0700

    use extend in bin/fullnode.js; make executable

--- bin/fullnode.js
+++ bin/fullnode.js
@@ -1,12 +1,9 @@
+#!/usr/bin/env node
 "use strict";
 let repl = require('repl');
 let fullnode = require('../');
 // Make all fullnode classes globally available.
-for (let prop in fullnode) {
-  global[prop] = fullnode[prop];
-}
-// And also, to easily view what things are available...
-global['fullnode'] = fullnode;
+fullnode.extend(global, fullnode, {fullnode: fullnode});
 repl.start({
   prompt: 'fullnode> ',
   useGlobal: true,
