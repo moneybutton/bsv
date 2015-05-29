801100415b10fd66c743fb02649b181fc68b7832
Ryan X. Charles <ryanxcharles@gmail.com>
2015-05-29 07:50:42 -0700

    move BIP39 to own dir; convert to inject pattern
    In order to make it possible for people to include only the English or Japanese
    or other word lists, I'm moving BIP39 to its own folder along with two new
    files for the wordlists, en.js and jp.js, and migrating to the injector
    pattern.  Both wordlists are still included, but now it's possible for people
    to override them with other wordlists externally. I will probably ultimately
    remove the Japanese wordlist from the defaults as well.
    
    I've also migrated toMnemonic/fromMnemonic to toString/fromString which is more
    idiomatic.

--- test/bip39.js
+++ test/bip39.js
@@ -9,32 +9,33 @@ describe('BIP39', function() {
   it('should initialize the class', function() {
     should.exist(BIP39);
     should.exist(BIP39());
-    should.exist(BIP39('en'));
-    should.exist(BIP39('jp'));
+    should.exist(BIP39.en);
+    should.exist(BIP39.jp);
   });
 
   it('should have a wordlist of length 2048', function() {
-    BIP39.wordlist_en.length.should.equal(2048);
+    require('../lib/bip39/en').length.should.equal(2048);
+    require('../lib/bip39/jp').length.should.equal(2048);
   });
 
   it('should generate a mnemonic phrase that passes the check', function() {
     let mnemonic;
 
     // should be able to make a mnemonic with or without the default wordlist
-    let bip39 = BIP39('en').fromRandom(128);
+    let bip39 = BIP39.en().fromRandom(128);
     bip39.check().should.equal(true);
     bip39 = BIP39().fromRandom(128);
     bip39.check().should.equal(true);
 
     let entropy = new Buffer(32);
     entropy.fill(0);
-    bip39 = BIP39('en').fromEntropy(entropy);
+    bip39 = BIP39.en().fromEntropy(entropy);
     bip39.check().should.equal(true);
 
     mnemonic = bip39.mnemonic;
 
     // mnemonics with extra whitespace do not pass the check
-    bip39 = BIP39('en').fromMnemonic(mnemonic + ' ');
+    bip39 = BIP39.en().fromString(mnemonic + ' ');
     bip39.check().should.equal(false);
 
     // mnemonics with a word replaced do not pass the check
@@ -42,7 +43,7 @@ describe('BIP39', function() {
     words[words.length - 1].should.not.equal('zoo');
     words[words.length - 1] = 'zoo';
     mnemonic = words.join(' ');
-    BIP39('en').fromMnemonic(mnemonic).check().should.equal(false);
+    BIP39.en().fromString(mnemonic).check().should.equal(false);
   });
 
   describe('#fromRandom', function() {
@@ -75,7 +76,7 @@ describe('BIP39', function() {
       let buf = new Buffer(128 / 8);
       buf.fill(0);
       let mnemonic1 = BIP39().entropy2mnemonic(buf).mnemonic;
-      let mnemonic2 = BIP39('en').entropy2mnemonic(buf).mnemonic;
+      let mnemonic2 = BIP39.en().entropy2mnemonic(buf).mnemonic;
       mnemonic1.should.equal(mnemonic2);
     });
 
@@ -87,17 +88,17 @@ describe('BIP39', function() {
       let buf = new Buffer(128 / 8);
       buf.fill(0);
       let mnemonic = BIP39().entropy2mnemonic(buf).mnemonic;
-      BIP39().fromMnemonic(mnemonic).check().should.equal(true);
-      BIP39('en').fromMnemonic(mnemonic).check().should.equal(true);
+      BIP39().fromString(mnemonic).check().should.equal(true);
+      BIP39.en().fromString(mnemonic).check().should.equal(true);
     });
 
   });
 
-  describe('#fromMnemonic', function() {
+  describe('#fromString', function() {
 
     it('should throw an error in invalid mnemonic', function() {
       (function() {
-        BIP39().fromMnemonic("invalid mnemonic").toSeed();
+        BIP39().fromString("invalid mnemonic").toSeed();
       }).should.throw('Mnemonic does not pass the check - was the mnemonic typed incorrectly? Are there extra spaces?');
     });
 
@@ -108,8 +109,8 @@ describe('BIP39', function() {
     vectors.english.forEach(function(vector, v) {
       it('should pass english test vector ' + v, function() {
         let entropy = new Buffer(vector.entropy, 'hex');
-        let bip39 = BIP39('en').fromEntropy(entropy)
-        bip39.toMnemonic().should.equal(vector.mnemonic);
+        let bip39 = BIP39.en().fromEntropy(entropy)
+        bip39.toString().should.equal(vector.mnemonic);
         bip39.check().should.equal(true);
         let seed = bip39.toSeed(vector.passphrase);
         seed.toString('hex').should.equal(vector.seed);
@@ -120,8 +121,8 @@ describe('BIP39', function() {
     vectors.japanese.forEach(function(vector, v) {
       it('should pass japanese test vector ' + v, function() {
         let entropy = new Buffer(vector.entropy, 'hex');
-        let bip39 = BIP39('jp').fromEntropy(entropy)
-        bip39.toMnemonic().should.equal(vector.mnemonic);
+        let bip39 = BIP39.jp().fromEntropy(entropy)
+        bip39.toString().should.equal(vector.mnemonic);
         bip39.check().should.equal(true);
         let seed = bip39.toSeed(vector.passphrase);
         seed.toString('hex').should.equal(vector.seed);
