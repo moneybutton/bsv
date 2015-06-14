20ea7951b72fa474661dfc9a1aa65f307568c557
Ryan X. Charles <ryanxcharles@gmail.com>
2015-06-14 13:22:29 -0700

    make test html main index.html file

--- testapp.js
+++ testapp.js
@@ -13,6 +13,6 @@ let server = app.listen(3000, function () {
   let host = server.address().address;
   let port = server.address().port;
 
-  console.log('Run the mocha tests at http://%s:%s/test.html', host, port);
+  console.log('Run the mocha tests at http://%s:%s/', host, port);
 
 });
