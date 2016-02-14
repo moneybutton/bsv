4c6b347a1b286abd110c3261f91532b22d8c86b2
Ryan X. Charles <ryanxcharles@gmail.com>
2016-02-14 14:14:24 -0800

    WorkersCmd & WorkersResult
    I'm going to change the way we send data to and from a worker. Rather than send
    JSON, we'll send a buffer. The WorkersCmd is the tool used to create a message
    for a worker, and WorkersResult is what they send back.

---
+++ test/workers-cmd.js
@@ -0,0 +1,48 @@
+/* global describe,it */
+'use strict'
+let BIP32 = require('../lib/bip32')
+let WorkersCmd = require('../lib/workers-cmd')
+let cmp = require('../lib/cmp')
+let should = require('chai').should()
+
+describe('WorkersCmd', function () {
+
+  it('should satisfy this basic API', function () {
+    let workersCmd = WorkersCmd()
+    should.exist(workersCmd)
+  })
+
+  describe('#fromMethod', function () {
+    it('should convert a bip32 into a workerscmd', function () {
+      let bip32 = BIP32().fromRandom()
+      let args = []
+      let workersCmd = WorkersCmd().fromMethod(bip32, 'toString', [], 0)
+      cmp(workersCmd.objbuf, bip32.toFastBuffer()).should.equal(true)
+      workersCmd.args.length.should.equal(0)
+      workersCmd.id.should.equal(0)
+    })
+  })
+
+  describe('#toBuffer', function () {
+    it('should convert a bip32 into a workerscmd', function () {
+      let bip32 = BIP32().fromRandom()
+      let args = []
+      let workersCmd = WorkersCmd().fromMethod(bip32, 'toString', [], 0)
+      workersCmd.toBuffer().length.should.greaterThan(0)
+    })
+  })
+
+  describe('#fromBuffer', function () {
+    it('should read a buffer', function () {
+      let bip32 = BIP32().fromRandom()
+      let args = []
+      let workersCmd = WorkersCmd().fromMethod(bip32, 'toString', [], 0)
+      let buf = workersCmd.toBuffer()
+      workersCmd = WorkersCmd().fromBuffer(buf)
+      cmp(workersCmd.objbuf, bip32.toFastBuffer()).should.equal(true)
+      workersCmd.args.length.should.equal(0)
+      workersCmd.id.should.equal(0)
+    })
+  })
+
+})
