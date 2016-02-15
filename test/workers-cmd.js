2b66b6dd097572856547d0e28c2fbb48e4e2ba0e
Ryan X. Charles <ryanxcharles@gmail.com>
2016-02-14 22:04:01 -0800

    support sending complex arguments to workers
    It was the case that you could only send standard javascript types in the
    arguments to the workers, but this was insufficient, because you might want to
    send buffers or fullnode objects in the arguments. I have now added support for
    sending complex arguments, and also some tests by using this functionality for
    public keys. You can now derive a public key from a private key using this
    strategy. I needed to add support for to/fromFastBuffer for pubkeys for this to
    work, in particular zero sized, i.e. blank, public keys.

--- test/workers-cmd.js
+++ test/workers-cmd.js
@@ -1,6 +1,7 @@
 /* global describe,it */
 'use strict'
 let BIP32 = require('../lib/bip32')
+let BW = require('../lib/bw')
 let WorkersCmd = require('../lib/workers-cmd')
 let cmp = require('../lib/cmp')
 let should = require('chai').should()
@@ -20,14 +21,48 @@ describe('WorkersCmd', function () {
       workersCmd.args.length.should.equal(0)
       workersCmd.id.should.equal(0)
     })
+
+    it('should convert a bip32 into a workerscmd with complicated args', function () {
+      let bip32 = BIP32().fromRandom()
+      let arg0 = true
+      let arg1 = new Buffer(5)
+      arg1.fill(0)
+      let arg2 = BIP32().fromRandom()
+      let args = [arg0, arg1, arg2]
+      let workersCmd = WorkersCmd().fromMethod(bip32, 'toString', args, 0)
+      cmp(workersCmd.objbuf, bip32.toFastBuffer()).should.equal(true)
+      workersCmd.args.length.should.equal(3)
+      workersCmd.args[0].should.equal(arg0)
+      cmp(workersCmd.args[1], arg1).should.equal(true)
+      workersCmd.args[2].toString().should.equal(arg2.toString())
+      workersCmd.id.should.equal(0)
+    })
   })
 
   describe('#toBuffer', function () {
-    it('should convert a bip32 into a workerscmd', function () {
+    it('should convert a bip32 into a workerscmd buffer', function () {
       let bip32 = BIP32().fromRandom()
       let args = []
       let workersCmd = WorkersCmd().fromMethod(bip32, 'toString', args, 0)
       workersCmd.toBuffer().length.should.greaterThan(0)
+    })
+
+    it('should convert a bip32 into a workerscmd buffer with complicated args', function () {
+      let bip32 = BIP32().fromRandom()
+      let arg0 = true
+      let arg1 = new Buffer(5)
+      arg1.fill(0)
+      let arg2 = BIP32().fromRandom()
+      let args = [arg0, arg1, arg2]
+      let workersCmd = WorkersCmd().fromMethod(bip32, 'toString', args, 0)
+      let buf = workersCmd.toBuffer(BW())
+      workersCmd = WorkersCmd().fromBuffer(buf, {BIP32})
+      cmp(workersCmd.objbuf, bip32.toFastBuffer()).should.equal(true)
+      workersCmd.args.length.should.equal(3)
+      workersCmd.args[0].should.equal(arg0)
+      cmp(workersCmd.args[1], arg1).should.equal(true)
+      workersCmd.args[2].toString().should.equal(arg2.toString())
+      workersCmd.id.should.equal(0)
     })
   })
 
--- test/workers.js
+++ test/workers.js
@@ -3,6 +3,8 @@
 let asink = require('asink')
 let BIP32 = require('../lib/bip32')
 let Workers = require('../lib/workers')
+let Privkey = require('../lib/privkey')
+let Pubkey = require('../lib/pubkey')
 let should = require('chai').should()
 
 describe('Workers', function () {
@@ -19,6 +21,16 @@ describe('Workers', function () {
         let workersResult = yield Workers.asyncObjectMethod(bip32, 'toString', [])
         let str = JSON.parse(workersResult.resbuf.toString())
         str[0].should.equal('x')
+      }, this)
+    })
+
+    it('should compute this method with fullnode object in args in the workers', function () {
+      return asink(function *() {
+        let privkey = Privkey().fromRandom()
+        let pubkey1 = Pubkey().fromPrivkey(privkey)
+        let workersResult = yield Workers.asyncObjectMethod(Pubkey(), 'fromPrivkey', [privkey])
+        let pubkey2 = Pubkey().fromFastBuffer(workersResult.resbuf)
+        pubkey1.toString().should.equal(pubkey2.toString())
       }, this)
     })
   })
