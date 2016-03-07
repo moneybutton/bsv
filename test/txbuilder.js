67b87580ed22483e565093228afd92ff28baa827
Ryan X. Charles <ryanxcharles@gmail.com>
2016-03-07 14:52:08 -0800

    txbuilder async sign
    Signing an input is a blocking operation, so it should be put in a worker.
    There is one difference between this new method and most other "async" methods
    that put stuff in workers, which is we are not simply calling a synchronous
    method inside a worker, but rather reimplementing some of the same code in the
    asynchronous version. The reason for this is that a Txbuilder object is a large
    object, and we don't want to send the entire thing to the worker. All we want
    to do is sign the one input, which requires sending a transaction to the
    worker, but not all the other properties of a Txbuilder object (such as all
    unspent txouts). This is actually one of the primary reasons why the new worker
    interface works the way it does - only a Txbuilder object knows how best to
    send its own data to a worker to perform an operation.

--- test/txbuilder.js
+++ test/txbuilder.js
@@ -1,17 +1,18 @@
 /* global describe,it */
 'use strict'
-let should = require('chai').should()
-let Txbuilder = require('../lib/txbuilder')
-let Txverifier = require('../lib/txverifier')
-let Tx = require('../lib/tx')
-let Txout = require('../lib/txout')
 let Address = require('../lib/address')
 let BN = require('../lib/bn')
 let Interp = require('../lib/interp')
-let Script = require('../lib/script')
-let Pubkey = require('../lib/pubkey')
-let Privkey = require('../lib/privkey')
 let Keypair = require('../lib/keypair')
+let Privkey = require('../lib/privkey')
+let Pubkey = require('../lib/pubkey')
+let Script = require('../lib/script')
+let Tx = require('../lib/tx')
+let Txbuilder = require('../lib/txbuilder')
+let Txout = require('../lib/txout')
+let Txverifier = require('../lib/txverifier')
+let asink = require('asink')
+let should = require('chai').should()
 
 describe('Txbuilder', function () {
   let txb
@@ -30,59 +31,67 @@ describe('Txbuilder', function () {
   })
 
   it('should make a new tx following this API', function () {
-    txb = new Txbuilder()
+    return asink(function *() {
+      txb = new Txbuilder()
 
-    // make change address
-    let privkey = Privkey().fromBN(BN(1))
-    let keypair = Keypair().fromPrivkey(privkey)
-    let changeaddr = Address().fromPubkey(keypair.pubkey)
+      // make change address
+      let privkey = Privkey().fromBN(BN(1))
+      let keypair = Keypair().fromPrivkey(privkey)
+      let changeaddr = Address().fromPubkey(keypair.pubkey)
 
-    // make addresses to send from
-    let privkey1 = Privkey().fromBN(BN(2))
-    let keypair1 = Keypair().fromPrivkey(privkey1)
-    let addr1 = Address().fromPubkey(keypair1.pubkey)
+      // make addresses to send from
+      let privkey1 = Privkey().fromBN(BN(2))
+      let keypair1 = Keypair().fromPrivkey(privkey1)
+      let addr1 = Address().fromPubkey(keypair1.pubkey)
 
-    let privkey2 = Privkey().fromBN(BN(3))
-    let keypair2 = Keypair().fromPrivkey(privkey2)
-    let addr2 = Address().fromPubkey(keypair2.pubkey)
+      let privkey2 = Privkey().fromBN(BN(3))
+      let keypair2 = Keypair().fromPrivkey(privkey2)
+      let addr2 = Address().fromPubkey(keypair2.pubkey)
 
-    // make addresses to send to
-    let saddr1 = addr1
-    let saddr2 = Address().fromRedeemScript(Script().fromString('OP_RETURN')) // fake, unredeemable p2sh address
+      // make addresses to send to
+      let saddr1 = addr1
+      let saddr2 = Address().fromRedeemScript(Script().fromString('OP_RETURN')) // fake, unredeemable p2sh address
 
-    // txouts that we are spending
-    let scriptout1 = Script().fromString('OP_DUP OP_HASH160 20 0x' + addr1.hashbuf.toString('hex') + ' OP_EQUALVERIFY OP_CHECKSIG')
-    let scriptout2 = Script().fromString('OP_DUP OP_HASH160 20 0x' + addr2.hashbuf.toString('hex') + ' OP_EQUALVERIFY OP_CHECKSIG')
-    let txout1 = Txout(BN(1e8), scriptout1)
-    let txout2 = Txout(BN(1e8 + 0.001e8), scriptout2) // contains extra that we will use for the fee
+      // txouts that we are spending
+      let scriptout1 = Script().fromString('OP_DUP OP_HASH160 20 0x' + addr1.hashbuf.toString('hex') + ' OP_EQUALVERIFY OP_CHECKSIG')
+      let scriptout2 = Script().fromString('OP_DUP OP_HASH160 20 0x' + addr2.hashbuf.toString('hex') + ' OP_EQUALVERIFY OP_CHECKSIG')
+      let txout1 = Txout(BN(1e8), scriptout1)
+      let txout2 = Txout(BN(1e8 + 0.001e8), scriptout2) // contains extra that we will use for the fee
 
-    let txhashbuf = new Buffer(32)
-    txhashbuf.fill(0)
-    let txoutnum1 = 0
-    let txoutnum2 = 1
+      let txhashbuf = new Buffer(32)
+      txhashbuf.fill(0)
+      let txoutnum1 = 0
+      let txoutnum2 = 1
 
-    txb.setFeePerKBNum(0.0001e8)
-    txb.setChangeAddress(changeaddr)
-    txb.from(txhashbuf, txoutnum1, txout1, keypair1.pubkey)
-    txb.from(txhashbuf, txoutnum2, txout2, keypair2.pubkey)
-    txb.to(BN(1e8), saddr1) // pubkeyhash address
-    txb.to(BN(1e8), saddr2) // p2sh address
-    // txb.randomizeInputs()
-    // txb.randomizeOutputs()
+      txb.setFeePerKBNum(0.0001e8)
+      txb.setChangeAddress(changeaddr)
+      txb.from(txhashbuf, txoutnum1, txout1, keypair1.pubkey)
+      txb.from(txhashbuf, txoutnum2, txout2, keypair2.pubkey)
+      txb.to(BN(1e8), saddr1) // pubkeyhash address
+      txb.to(BN(1e8), saddr2) // p2sh address
+      // txb.randomizeInputs()
+      // txb.randomizeOutputs()
 
-    txb.build()
-    txb.sign(0, keypair1)
-    txb.sign(1, keypair2)
+      txb.build()
+      txb.sign(0, keypair1)
+      txb.sign(1, keypair2)
 
-    txb.tx.txouts[0].script.chunks[2].buf.toString('hex').should.equal(saddr1.hashbuf.toString('hex'))
-    txb.tx.txouts[0].valuebn.eq(1e8).should.equal(true)
-    txb.tx.txouts[1].valuebn.eq(1e8).should.equal(true)
-    txb.tx.txouts[2].valuebn.gt(0).should.equal(true)
-    txb.tx.txouts[2].valuebn.gt(546).should.equal(true)
-    txb.tx.txouts[2].valuebn.eq(0.001e8 - 0.0001e8).should.equal(true)
-    txb.tx.txouts[2].script.chunks[2].buf.toString('hex').should.equal(changeaddr.hashbuf.toString('hex'))
+      txb.tx.txouts[0].script.chunks[2].buf.toString('hex').should.equal(saddr1.hashbuf.toString('hex'))
+      txb.tx.txouts[0].valuebn.eq(1e8).should.equal(true)
+      txb.tx.txouts[1].valuebn.eq(1e8).should.equal(true)
+      txb.tx.txouts[2].valuebn.gt(0).should.equal(true)
+      txb.tx.txouts[2].valuebn.gt(546).should.equal(true)
+      txb.tx.txouts[2].valuebn.eq(0.001e8 - 0.0001e8).should.equal(true)
+      txb.tx.txouts[2].script.chunks[2].buf.toString('hex').should.equal(changeaddr.hashbuf.toString('hex'))
 
-    Txverifier.verify(txb.tx, txb.utxoutmap, Interp.SCRIPT_VERIFY_P2SH).should.equal(true)
+      Txverifier.verify(txb.tx, txb.utxoutmap, Interp.SCRIPT_VERIFY_P2SH).should.equal(true)
+
+      // or:
+      yield txb.asyncSign(0, keypair1)
+      yield txb.asyncSign(1, keypair2)
+
+      Txverifier.verify(txb.tx, txb.utxoutmap, Interp.SCRIPT_VERIFY_P2SH).should.equal(true)
+    }, this)
   })
 
   describe('#toJSON', function () {
