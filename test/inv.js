c4320d236a23892c9f270345e1cdd7bb07d2dbbc
Ryan X. Charles <ryanxcharles@gmail.com>
2016-04-09 13:19:24 -0700

    convenience methods for checking inv types

--- test/inv.js
+++ test/inv.js
@@ -35,4 +35,31 @@ describe('Inv', function () {
       Buffer.compare(buf, buf2).should.equal(0)
     })
   })
+
+  describe('#isTx', function () {
+    it('should know this is a tx hash', function () {
+      let hashbuf = Hash.sha256(new Buffer(0))
+      let typenum = Inv.MSG_TX
+      let inv = Inv(typenum, hashbuf)
+      inv.isTx().should.equal(true)
+    })
+  })
+
+  describe('#isBlock', function () {
+    it('should know this is a block hash', function () {
+      let hashbuf = Hash.sha256(new Buffer(0))
+      let typenum = Inv.MSG_BLOCK
+      let inv = Inv(typenum, hashbuf)
+      inv.isBlock().should.equal(true)
+    })
+  })
+
+  describe('#isFilteredBlock', function () {
+    it('should know this is a filtered block hash', function () {
+      let hashbuf = Hash.sha256(new Buffer(0))
+      let typenum = Inv.MSG_FILTERED_BLOCK
+      let inv = Inv(typenum, hashbuf)
+      inv.isFilteredBlock().should.equal(true)
+    })
+  })
 })
