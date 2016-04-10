f572a7b470f47e2398432359a04c0d7660a0a2fd
Ryan X. Charles <ryanxcharles@gmail.com>
2016-04-10 11:19:11 -0700

    MsgAddr

---
+++ test/addr.js
@@ -0,0 +1,39 @@
+/* global describe,it */
+'use strict'
+let Addr = require('../lib/addr')
+let should = require('chai').should()
+
+describe('Addr', function () {
+  it('should exist', function () {
+    should.exist(Addr)
+    should.exist(Addr())
+  })
+
+  describe('#toBuffer', function () {
+    it('should convert an addr to a buffer', function () {
+      let addr = Addr().fromObject({
+        time: 1000,
+        servicesbuf: new Buffer(8).fill(0),
+        ipaddrbuf: new Buffer(16).fill(0),
+        port: 8333
+      })
+      addr.toBuffer().length.should.equal(4 + 8 + 16 + 2)
+    })
+  })
+
+  describe('#toBuffer', function () {
+    it('should convert an addr to a buffer', function () {
+      let addr = Addr().fromObject({
+        time: 1000,
+        servicesbuf: new Buffer(8).fill(0),
+        ipaddrbuf: new Buffer(16).fill(0),
+        port: 8333
+      })
+      let addr2 = Addr().fromBuffer(addr.toBuffer())
+      addr2.time.should.equal(addr.time)
+      Buffer.compare(addr.servicesbuf, addr2.servicesbuf).should.equal(0)
+      Buffer.compare(addr.ipaddrbuf, addr2.ipaddrbuf).should.equal(0)
+      addr2.port.should.equal(addr.port)
+    })
+  })
+})
---
+++ test/msgaddr.js
@@ -0,0 +1,49 @@
+/* global describe,it */
+'use strict'
+let Addr = require('../lib/addr')
+let MsgAddr = require('../lib/msgaddr')
+let should = require('chai').should()
+
+describe('MsgAddr', function () {
+  let addrhex = 'e8030000000000000000000000000000000000000000000000000000208d'
+
+  it('should exist', function () {
+    should.exist(MsgAddr)
+    should.exist(MsgAddr())
+  })
+
+  describe('#fromAddrs', function () {
+    it('should convert from addrs', function () {
+      let addr = Addr().fromHex(addrhex)
+      let msgaddr = MsgAddr().fromAddrs([addr])
+      msgaddr.getCmd().should.equal('addr')
+      msgaddr.databuf.length.should.equal(1 + addrhex.length / 2)
+    })
+  })
+
+  describe('#toAddrs', function () {
+    it('should convert to addrs', function () {
+      let addr = Addr().fromHex(addrhex)
+      let msgaddr = MsgAddr().fromAddrs([addr])
+      let addrs2 = msgaddr.toAddrs()
+      addrs2.length.should.equal(1)
+      ;(addrs2[0] instanceof Addr).should.equal(true)
+    })
+
+    it('should convert to multiple addrs', function () {
+      let addr = Addr().fromHex(addrhex)
+      let msgaddr = MsgAddr().fromAddrs([addr, addr, addr])
+      let addrs2 = msgaddr.toAddrs()
+      addrs2.length.should.equal(3)
+      ;(addrs2[0] instanceof Addr).should.equal(true)
+    })
+  })
+
+  describe('#isValid', function () {
+    it('should know this is a valid msg addrs', function () {
+      let addr = Addr().fromHex(addrhex)
+      let msgaddr = MsgAddr().fromAddrs([addr])
+      msgaddr.isValid().should.equal(true)
+    })
+  })
+})
