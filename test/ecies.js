62cb6650de9d422e09bed93ae64f7cda0552df93
Ryan X. Charles <ryanxcharles@gmail.com>
2016-05-12 15:05:03 +0200

    ecies async encrypt and decrypt
    ECIES is a standard algorithm to encrypt data to someone using their EC public
    key. This is extremely slow because of multiple rounds of point multiplication
    and hashing that has to occur to do this. I've now added async versions of both
    encrypting and decrypting so it can be used in a UI where responsiveness
    matters.

--- test/ecies.js
+++ test/ecies.js
@@ -4,6 +4,7 @@ let Ecies = require('../lib/ecies')
 let should = require('chai').should()
 let KeyPair = require('../lib/key-pair')
 let Hash = require('../lib/hash')
+let asink = require('asink')
 
 describe('#Ecies', function () {
   it('should make a new Ecies object', function () {
@@ -26,6 +27,22 @@ describe('#Ecies', function () {
     })
   })
 
+  describe('@asyncEncrypt', function () {
+    it('should return a buffer', function () {
+      return asink(function * () {
+        let encBuf = yield Ecies.asyncEncrypt(messageBuf, tokey.pubKey, fromkey)
+        Buffer.isBuffer(encBuf).should.equal(true)
+      }, this)
+    })
+
+    it('should return a buffer if fromkey is not present', function () {
+      return asink(function * () {
+        let encBuf = yield Ecies.asyncEncrypt(messageBuf, tokey.pubKey)
+        Buffer.isBuffer(encBuf).should.equal(true)
+      }, this)
+    })
+  })
+
   describe('@decrypt', function () {
     it('should decrypt that which was encrypted', function () {
       let encBuf = Ecies.encrypt(messageBuf, tokey.pubKey, fromkey)
@@ -37,6 +54,24 @@ describe('#Ecies', function () {
       let encBuf = Ecies.encrypt(messageBuf, tokey.pubKey)
       let messageBuf2 = Ecies.decrypt(encBuf, tokey.privKey)
       messageBuf2.toString('hex').should.equal(messageBuf.toString('hex'))
+    })
+  })
+
+  describe('@asyncDecrypt', function () {
+    it('should decrypt that which was encrypted', function () {
+      return asink(function * () {
+        let encBuf = yield Ecies.asyncEncrypt(messageBuf, tokey.pubKey, fromkey)
+        let messageBuf2 = Ecies.decrypt(encBuf, tokey.privKey)
+        messageBuf2.toString('hex').should.equal(messageBuf.toString('hex'))
+      }, this)
+    })
+
+    it('should decrypt that which was encrypted if fromKeyPair was randomly generated', function () {
+      return asink(function * () {
+        let encBuf = yield Ecies.asyncEncrypt(messageBuf, tokey.pubKey)
+        let messageBuf2 = Ecies.decrypt(encBuf, tokey.privKey)
+        messageBuf2.toString('hex').should.equal(messageBuf.toString('hex'))
+      }, this)
     })
   })
 })
