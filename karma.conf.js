7f4b4468332d218b923e2aaa8ab38ea4b0f78ca3
Ryan X. Charles <ryanxcharles@gmail.com>
2019-01-25 21:24:19 -0800

    more automatic linter fixes - mostly remove semicolons

--- karma.conf.js
+++ karma.conf.js
@@ -1,24 +1,23 @@
-'use strict';
+'use strict'
 
 // karma.conf.js
-module.exports = function(config) {
-
+module.exports = function (config) {
   config.set({
     browsers: ['Firefox'],
     frameworks: ['mocha', 'detectBrowsers'],
     detectBrowsers: {
       enabled: true,
       usePhantomJS: false,
-      postDetection: function(availableBrowser) {
+      postDetection: function (availableBrowser) {
         // modify to enable additional browsers if available
-        var runBrowsers = ['Firefox', 'Chrome'];
-        var browsers = [];
-        for(var i = 0; i < runBrowsers.length; i++) {
-          if(~availableBrowser.indexOf(runBrowsers[i])) {
-            browsers.push(runBrowsers[i]);
+        var runBrowsers = ['Firefox', 'Chrome']
+        var browsers = []
+        for (var i = 0; i < runBrowsers.length; i++) {
+          if (~availableBrowser.indexOf(runBrowsers[i])) {
+            browsers.push(runBrowsers[i])
           }
         }
-        return browsers;
+        return browsers
       }
     },
     singleRun: true,
@@ -31,6 +30,5 @@ module.exports = function(config) {
       'karma-firefox-launcher',
       'karma-detect-browsers'
     ]
-  });
-
-};
+  })
+}
