cf39884aa03f57b1f3bb0451870635e372ceee9d
Dave Foderick <dfoderick@gmail.com>
2019-04-18 16:11:58 -0700

    Transaction input isFinal (#82)
    * fix Transaction.Input.isFinal
    
    * fix interpreter CTLV
    
    * revert interpreter test changes
    
    * fix yarn test complaint

--- test/transaction/input/input.js
+++ test/transaction/input/input.js
@@ -81,4 +81,12 @@ describe('Transaction.Input', function () {
     var input = new Input(output)
     input._estimateSize().should.equal(66)
   })
+
+  it('defaults isFinal correctly', function () {
+    new Input(output).isFinal().should.equal(true)
+  })
+
+  it('handles isFinal when sequence number is set to non default value', function () {
+    new Input({ ...output, sequenceNumber: 0 }).isFinal().should.equal(false)
+  })
 })
