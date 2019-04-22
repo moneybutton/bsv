34f8de9ac5e62b99b793e8fa82bb098b328af55a
Monkeylord <bluegod_001@126.com>
2019-04-23 00:18:26 +0800

    Optimization for size of bsv.min.js (#78)
    * lodash optimizition
    
    * successfully reduced bsv.min.js to 351 KB by remove nodejs crypto library
    
    * add a switch for Hash in browser/node, revert Base58 to bs58
    
    * add bs58 dep back in package.json
    
    * add hash.browser test

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+var path = require('path')
+
+module.exports = {
+  entry: path.join(__dirname, '/index.js'),
+  externals: {
+    crypto: 'crypto'
+  },
+  output: {
+    library: 'bsv',
+    path: path.join(__dirname, '/'),
+    filename: 'bsv.min.js'
+  }
+}
