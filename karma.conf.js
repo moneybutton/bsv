f500434e30d0cf257f959b671a02ec0f3cb5b360
hojarasca <miguelfeliped@gmail.com>
2020-05-16 20:32:19 -0300

    Added karma.

--- karma.conf.js
+++ karma.conf.js
@@ -1,67 +1,70 @@
 // Karma configuration
-// Generated on Tue Jan 06 2015 16:30:03 GMT-0800 (PST)
+// Generated on Sat May 16 2020 20:12:10 GMT-0300 (Argentina Standard Time)
 
-module.exports = function(config) {
+module.exports = function (config) {
   config.set({
 
     // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: 'build',
-
+    basePath: '',
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['mocha'],
 
-
     // list of files / patterns to load in the browser
     files: [
-      {pattern: 'bsv.js', watched: true, included: true, served: true},
-      {pattern: 'bsv-worker.js', watched: true, included: false, served: true},
-      'tests.js'
+      'test/**/*.js'
     ],
 
-
-    // list of files to exclude
+    // list of files / patterns to exclude
     exclude: [
     ],
-
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      'test/**/*.js': ['webpack']
     },
 
+    plugins: [
+      require('karma-chrome-launcher'),
+      require('karma-firefox-launcher'),
+      require('karma-mocha'),
+      require('karma-webpack')
+    ],
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
     reporters: ['progress'],
 
-
     // web server port
     port: 9876,
-
 
     // enable / disable colors in the output (reporters and logs)
     colors: true,
 
-
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_DEBUG,
-
+    logLevel: config.LOG_INFO,
 
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: false,
 
-
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Firefox'],
-
+    browsers: ['ChromeHeadless', 'Firefox'],
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: true
-  });
-};
+    singleRun: false,
+
+    webpack: {
+      mode: 'production'
+    },
+
+    // Concurrency level
+    // how many browser should be started simultaneous
+    concurrency: Infinity
+  })
+}
