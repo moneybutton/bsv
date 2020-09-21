8ba6c30e9cb3d035d9bdc700424c108baede1e65
zhanglinjie <zhanglinjie@big.one>
2020-09-21 15:27:04 +0800

    remove log

--- test/interp.js
+++ test/interp.js
@@ -430,7 +430,6 @@ describe('Interp', function () {
           }
         } catch (err) {
           console.log(vector)
-          console.log(interp)
           throw new Error('failure', err)
         }
       })
